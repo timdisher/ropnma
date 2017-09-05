f17a9e51ebf8e382340303c8d4ee630bd7d8d0b4
timdisher <tim.disher@dal.ca>
2017-09-05 09:57:52 -0300

    cleaning

---
+++ analyses/final/rop_clean.R
@@ -0,0 +1,3 @@
+source("./analyses/final/rop_clean.R")
+
+str(rop_data_arm)
---
+++ analyses/final/rop_import.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+
+rop_data_study = read_csv("./data/rop_studylevel_aug.csv")
+
+rop_data_arm = read_csv("./data/rop_armlevel_aug.csv")
